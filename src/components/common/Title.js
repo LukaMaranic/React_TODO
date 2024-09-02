d2a6d99fd2be30a93852afa16d3964926eedfb04
LukaMaranic <luka.maranic@gmail.com>
2024-09-02 18:05:21 +0200

    Added Title component

---
+++ src/components/common/Title.js
@@ -0,0 +1,23 @@
+import { useLocation } from "react-router-dom";
+
+function Title() {
+  const location = useLocation();
+
+  const titles = {
+    "/": "Vehicle Models List",
+    "/vehicle-makes": "Vehicle Makes List",
+    "/manufactor-list": "Manufactor List",
+    "/new-manufactor": "Add New Manufactor",
+    "/new-vehicle": "Add New Vehicle Model",
+  };
+
+  const title = titles[location.pathname] || "";
+
+  return (
+    <h2 className="text-2xl font-semibold text-gray-700 dark:text-gray-200 my-4">
+      {title}
+    </h2>
+  );
+}
+
+export default Title;
