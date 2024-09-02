55532038fc0be343c176f8678186bf4c7197af22
LukaMaranic <luka.maranic@gmail.com>
2024-09-02 18:02:09 +0200

    Updated vehicle components

--- src/components/vehicle/NewVehicleForm.js
+++ src/components/vehicle/NewVehicleForm.js
@@ -1,13 +1,58 @@
 export default function NewVehicleForm() {
   return (
-    <form>
-      <label for="makeId">Make Id:</label>
-      <input type="text" id="makeId" name="makeId" />
-      <label for="manufactorName">Manufactor name:</label>
-      <input type="text" id="manufactorName" name="manufactorName" />
-      <label for="manufactorAbrv">Manufactor abbrivation:</label>
-      <input type="text" id="manufactorAbrv" name="manufactorAbrv" />
-      <input type="submit" value="Submit" />
+    <form class="mx-40 my-20">
+      <div class="grid gap-6 mb-6 md:grid-cols-2"></div>
+      <div class="mb-6">
+        <label
+          for="email"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Make Id:
+        </label>
+        <input
+          type="number"
+          id="makeId"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder=""
+          required
+        />
+      </div>
+      <div class="mb-6">
+        <label
+          for="password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Manufactor name:
+        </label>
+        <input
+          type="text"
+          id="manfName"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder=""
+          required
+        />
+      </div>
+      <div class="mb-6">
+        <label
+          for="confirm_password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Manufactor abbrivation:
+        </label>
+        <input
+          type="text"
+          id="manf_abrv"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
     </form>
   );
 }
--- src/components/vehicle/VehicleModelTable.js
+++ src/components/vehicle/VehicleModelTable.js
@@ -2,79 +2,190 @@ import { Link } from "react-router-dom";
 
 export default function VehicleModelTable() {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>MakeId</th>
-          <th>Name</th>
-          <th>Abrv</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>1</td>
-          <td>iX M60</td>
-          <td>BWM</td>
-          <td>
-            <button>
-              <Link to="/update-manufactor">Update manufactor</Link>
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>1</td>
-          <td>i5</td>
-          <td>BWM</td>
-          <td>
-            <button>
-              <Link to="/update-manufactor">Update manufactor</Link>
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td>2</td>
-          <td>Focus</td>
-          <td>Ford</td>
-          <td>
-            <button>
-              <Link to="/update-manufactor">Update manufactor</Link>
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <td>4</td>
-          <td>2</td>
-          <td>Mustang</td>
-          <td>Ford</td>
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>3</td>
-          <td>Golf</td>
-          <td>Volkswagenwerk GmbH</td>
-          <td>
-            <button>
-              <Link to="/update-manufactor">Update manufactor</Link>
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>3</td>
-          <td>Polo</td>
-          <td>Volkswagenwerk GmbH</td>
-          <td>
-            <button>
-              <Link to="/update-manufactor">Update manufactor</Link>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-9">
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" class="px-6 py-3">
+              <div class="flex items-center">
+                Id
+                <a href="#">
+                  <svg
+                    class="w-3 h-3 ms-1.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                  </svg>
+                </a>
+              </div>
+            </th>
+            <th scope="col" class="px-6 py-3">
+              <div class="flex items-center">
+                Make Id
+                <a href="#">
+                  <svg
+                    class="w-3 h-3 ms-1.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                  </svg>
+                </a>
+              </div>
+            </th>
+            <th scope="col" class="px-6 py-3">
+              <div class="flex items-center">
+                Name
+                <a href="#">
+                  <svg
+                    class="w-3 h-3 ms-1.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                  </svg>
+                </a>
+              </div>
+            </th>
+            <th scope="col" class="px-6 py-3">
+              <div class="flex items-center">
+                Abrv
+                <a href="#">
+                  <svg
+                    class="w-3 h-3 ms-1.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                  </svg>
+                </a>
+              </div>
+            </th>
+            <th scope="col" class="px-6 py-3">
+              <div class="flex items-center">
+                Action
+                <a href="#">
+                  <svg
+                    class="w-3 h-3 ms-1.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
+                  </svg>
+                </a>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              1
+            </th>
+            <td class="px-6 py-4">1</td>
+            <td class="px-6 py-4">iX M60</td>
+            <td class="px-6 py-4">BWM</td>
+            <td class="px-6 py-4">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              >
+                Edit
+              </a>
+            </td>
+          </tr>
+          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              2
+            </th>
+            <td class="px-6 py-4">1</td>
+            <td class="px-6 py-4">i5</td>
+            <td class="px-6 py-4">BMW</td>
+            <td class="px-6 py-4">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              >
+                Edit
+              </a>
+            </td>
+          </tr>
+          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              3
+            </th>
+            <td class="px-6 py-4">2</td>
+            <td class="px-6 py-4">Mustang</td>
+            <td class="px-6 py-4">Ford</td>
+            <td class="px-6 py-4">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              >
+                Edit
+              </a>
+            </td>
+          </tr>
+          <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              4
+            </th>
+            <td class="px-6 py-4">2</td>
+            <td class="px-6 py-4">Focus</td>
+            <td class="px-6 py-4">Ford</td>
+            <td class="px-6 py-4">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              >
+                Edit
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              5
+            </th>
+            <td class="px-6 py-4">3</td>
+            <td class="px-6 py-4">Golf</td>
+            <td class="px-6 py-4">Volkswagenwerk GmbH</td>
+            <td class="px-6 py-4">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              >
+                Edit
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
